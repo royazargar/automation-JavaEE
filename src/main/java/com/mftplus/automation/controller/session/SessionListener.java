b80ce92645d31f1188db44734c13c3115ab4b22c
kurromii <ahadzadeparnian@gmail.com>
2024-05-10 08:28:08 +0330

    session listener added -parnian

---
+++ src/main/java/com/mftplus/automation/controller/session/SessionListener.java
@@ -0,0 +1,30 @@
+package com.mftplus.automation.controller.session;
+
+import jakarta.servlet.http.HttpSessionEvent;
+import jakarta.servlet.http.HttpSessionListener;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SessionListener implements HttpSessionListener {
+    //for admin page
+
+    private static int online;
+    private static int visited;
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        log.info("SessionListener - session created");
+        online++;
+        visited++;
+        log.info("Num of Visited : " + visited);
+        log.info("Num of Online : " + online);
+        HttpSessionListener.super.sessionCreated(se);
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        log.info("SessionListener - session destroyed");
+        online--;
+        HttpSessionListener.super.sessionDestroyed(se);
+    }
+}
